5d70787770518f3852e136c3133c94e15a0397be
pedro fischer filho <pedro.fischer22@gmail.com>
2018-02-20 01:08:18 +0000

    Criando page de registro, falta corrigir action e route pois a página não aparece. Página principal aparentemente normal

--- app/Views/login/register.php
+++ app/Views/login/register.php
@@ -10,22 +10,11 @@
                 <div class="form-group">
                     <input type="password" name="senha" class="form-control" placeholder="Senha" maxlength="50" required>
                 </div>
-                <div class="form-group">
-                    <select class="form-control">
-                        <option value="4">Aluno</option>
-                        <option value="3">Orientador</option>
-                        <option value="2">Coordenador</option>
-                        <option value="1">Instituição</option>
-                    </select>
-                </div>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Acessar <i class="fa fa-check" aria-hidden="true"></i></button>
             </form>
             <a href="/facebook" class="btn btn-lg btn-primary btn-block mt-2" type="button">Logar com <i class="fa fa-facebook"></i></a>
         </div>
         <div class="col-md-4 col-xs-12 p-5 m-1 mb-4 bg-white text-primary rounded align-self-center shadow-md animated fadeInRight">
-            <p class="h2 text-center mb-4">Precisa de ajuda <i class="fa fa-question"></i></p>
-            <button class="btn btn-md btn-primary btn-block mt-2" type="button" id="solicitarAcesso">Solicitar acesso <i class="fa fa-user-plus" aria-hidden="true"></i></button>
-            <button class="btn btn-md btn-primary btn-block mt-2" type="button" >Reportar erro <i class="fa fa-bug" aria-hidden="true"></i></button>
         </div>
     </div>
 </div>
