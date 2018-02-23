0e26386c3d2f5aa69dab14b961e1c6d472602ed4
Lucas Viana <vianadossantoslucas@gmail.com>
2018-02-23 14:44:16 -0300

    [DELETE] - Conflitos

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< current
 <?php
 
     require 'vendor/autoload.php';
@@ -46,56 +45,3 @@
     });
 
     $app->run();
-
-?>
-=======
-<?php
-
-    require 'vendor/autoload.php';
-    require 'init.php';
-
-    $app = new \Slim\App([
-        'settings' => [
-            'displayErrorDetails' => true
-        ]
-    ]);
-
-    $app->get('/', function() {
-        // Renderiza Index
-    });
-
-    $app->post('/', function () {
-       // Envia formulário de Login
-    });
-
-    $app->get('/campanhas', function () {
-       // Exibe campanhas ativas
-    });
-
-    $app->get('/campanhas/nova', function () {
-        // Formulário de cadastro de novas campanhas
-    });
-
-    $app->post('/campanhas/nova', function() {
-        // Envio de dados do formulário de campanha
-    });
-
-    $app->get('/campanha/{id}', function () {
-        // $request->getAttribute('id') para pegar a váriavel da Url
-        // Exibe campanha e seus candidatos
-    });
-
-    $app->get('/campanha/{campanha}/{candidato}', function($request) {
-        // $request->getAttribute('id') para pegar a váriavel da Url
-        // exibe os dados do candidato
-    });
-
-    $app->post('/campanha/{campanha}/{candidato}', function() {
-        // Função de cadastro de votos
-        // Cadastra voto no Candidato
-    });
-
-    $app->run();
-
-?>
->>>>>>> before discard
