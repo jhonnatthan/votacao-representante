3121364216b021a6015441ebc872c5355793de92
Lucas Viana <vianadossantoslucas@gmail.com>
2018-02-23 14:43:22 -0300

    [ADD] - Criação de Campanha

--- app/Models/Campanha.php
+++ app/Models/Campanha.php
@@ -6,7 +6,19 @@ use App\Banco;
 class Campanha {
 
     public static function criaCampanha($nome, $descricao, $imagem = null, $anonimo = true, $dataInicio, $dataFim) {
+      $banco = new \App\Banco(); // Instância do objeto
+      $sql = "CALL campanha_insert(:nome, :descricao, :imagem, :anonimo, :dataInicio, :dataFim)"; // Query SQL
+      $stmt = $banco->prepare($sql); // Cria um Statement (Utilizdo para Query SQL)
+      /* Parâmetros da Procedure,
+        Isto é utilizado para segurança, evitando coisas como SQLInjection */
+      $stmt->bindParam(":nome", $nome, \PDO::PARAM_STR);
+      $stmt->bindParam(":descricao", $descricao, \PDO::PARAM_STR);
+      $stmt->bindParam(":imagem", $imagem, \PDO::PARAM_LOB);
+      $stmt->bindParam(":anonimo", $anonimo, \PDO::PARAM_BOOL);
+      $stmt->bindParam("dataInicio", $dataInicio, \PDO::PARAM_STR);
+      $stmt->bindParam(":dataFim", $dataFim, \PDO::PARAM_STR);
 
+      $stmt->execute();
     }
 
     public static function selecionaCampanha($codigoCampanha) {
