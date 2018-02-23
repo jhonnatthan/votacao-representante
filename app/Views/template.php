5f75d34cab126c89fda774763f462670f51ca8f5
Lucas Viana <vianadossantoslucas@gmail.com>
2018-02-23 17:59:11 -0300

    [UPDATE] - Adicionar <script> sem precisar ser um array()

--- app/Views/template.php
+++ app/Views/template.php
@@ -54,6 +54,8 @@
                 foreach ($scripts as $script) {
                     echo '<script src="' . $script . ' >';
                 }
+            } else {
+                echo '<script src="'.$scripts.'>';
             }
         }
 
